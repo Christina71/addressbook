093abc815b903ffaa165b3d12116f0c974d084ae
Christina71 <mariachristina71@yahoo.com>
2018-01-31 14:26:13 -0500

    Initial commit

---
+++ src/main/java/com/example/addressbook/Address.java
@@ -0,0 +1,71 @@
+package com.example.addressbook;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=2)
+    private String name;
+
+    @NotNull
+    @Size(min=5)
+    private String address;
+
+    @NotNull
+    @Size(min=10)
+    private String phone;
+
+    @NotNull
+    @Size(min=2)
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
